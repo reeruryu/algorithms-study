7b3cd74d506421cea085e93c0f108f70fe99537e
reeruryu <h2ju1004@gmail.com>
2023-07-13 14:40:58 +0900

    add: 1주차 스택/큐 문제 풀이

---
+++ src/reeruryu/P42584.java
@@ -0,0 +1,31 @@
+package reeruryu;
+
+/*
+문제: 프로그래머스 - 주식가격 / 난이도: Level 2
+ */
+
+import java.util.Stack;
+
+public class P42584 {
+    public int[] solution(int[] prices) {
+
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - idx - 1;
+        }
+
+        return answer;
+    }
+
+}
