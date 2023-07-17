ebb9c4a4089448239ea0fdc98b84f0cb121e7f93
reeruryu <h2ju1004@gmail.com>
2023-07-18 01:29:42 +0900

    add: 류희주 DP 문제 풀이

---
+++ src/reeruryu/week2/P42898.java
@@ -0,0 +1,26 @@
+package reeruryu.week2;
+
+/*
+문제: 프로그래머스 - 등굣길 / 난이도: Level 3
+ */
+
+public class P42898 {
+    public int solution(int m, int n, int[][] puddles) {
+        int mod = 1000000007;
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] puddle: puddles) {
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+
+        dp[1][1] = 1;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (dp[i][j] == -1) continue;
+                if (dp[i - 1][j] > 0) dp[i][j] += dp[i - 1][j] % mod;
+                if (dp[i][j - 1] > 0) dp[i][j] += dp[i][j - 1] % mod;
+            }
+        }
+        return dp[n][m] % mod;
+    }
+}
---
+++ src/reeruryu/week2/P43105.java
@@ -0,0 +1,29 @@
+package reeruryu.week2;
+
+/*
+문제: 프로그래머스 - 정수 삼각형 / 난이도: Level 3
+ */
+
+import java.util.*;
+
+public class P43105 {
+    public int solution(int[][] triangle) {
+
+        int i;
+        for (i = 0; i < triangle.length - 1; i++) {
+            for (int j = 0; j < triangle[i + 1].length; j++) {
+                int max = 0;
+                if (j == 0) {
+                    max = triangle[i][j];
+                } else if (j == triangle[i + 1].length - 1) {
+                    max = triangle[i][j - 1];
+                } else {
+                    max = Math.max(triangle[i][j - 1], triangle[i][j]);
+                }
+                triangle[i + 1][j] += max;
+            }
+        }
+
+        return Arrays.stream(triangle[i]).max().getAsInt();
+    }
+}
