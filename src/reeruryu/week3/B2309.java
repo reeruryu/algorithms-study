ac944d484310ed3180e2ec260154d1968f525064
reeruryu <h2ju1004@gmail.com>
2023-08-11 19:35:37 +0900

    add: 류희주 문제 풀이

---
+++ src/reeruryu/week3/B2309.java
@@ -0,0 +1,42 @@
+package reeruryu.week3;
+
+/*
+문제: 백준 - 일곱 난쟁이 / 난이도: 브론즈 2티어
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int[] nj = new int[9];
+        for (int i = 0; i < 9; i++) {
+            nj[i] = Integer.parseInt(br.readLine());
+            sum += nj[i];
+        }
+        Arrays.sort(nj);
+
+        // 40
+        // 7 8 10 13 15 19 20 23 25
+        int hap = sum - 100;
+        int left = 0, right = 8;
+        while (left < right) {
+            int cur = nj[left] + nj[right];
+            if (cur == hap) break;
+            else if (cur > hap) right--;
+            else left++;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (i == left || i == right) continue;
+            System.out.println(nj[i]);
+        }
+
+    }
+
+}
